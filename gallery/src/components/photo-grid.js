47382bca137edc7221f031f8a259c64fee09773e
Zoe Rizzo <rizzo.zoej@gmail.com>
2025-07-21 22:59:35 -0400

    many changes. start of actual photo gallery, unpolished

---
+++ gallery/src/components/photo-grid.js
@@ -0,0 +1,47 @@
+"use client";
+import Image from 'next/image';
+import { useState } from 'react';
+import PhotoModal from './photo-modal';
+
+export default function PhotoGrid({ photos }) {
+  const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(null);
+
+  const handleNext = () => {
+    if (selectedPhotoIndex !== null) {
+      setSelectedPhotoIndex((selectedPhotoIndex + 1) % photos.length);
+    }
+  };
+
+  const handlePrev = () => {
+    if (selectedPhotoIndex !== null) {
+      setSelectedPhotoIndex((selectedPhotoIndex - 1 + photos.length) % photos.length);
+    }
+  };
+
+  return (
+    <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-0">
+        {photos.map((photo, index) => (
+          <div key={photo.id} onClick={() => setSelectedPhotoIndex(index)} className="cursor-pointer w-full aspect-[1/1] relative">
+            <Image
+              src={photo.thumbnailUrl}
+              alt={photo.caption}
+              width={300}
+              height={300}
+              className="rounded shadow"
+            />
+          </div>
+        ))}
+      </div>
+
+      {selectedPhotoIndex !== null && (
+        <PhotoModal
+          photo={photos[selectedPhotoIndex]}
+          onClose={() => setSelectedPhotoIndex(null)}
+          onPrev={handlePrev}
+          onNext={handleNext}
+        />
+      )}
+    </div>
+  );
+}
---
+++ gallery/src/components/photo-modal.js
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+
+export default function PhotoModal({ photo, onClose, onPrev, onNext }) {
+  return (
+    /* TODO: header banner stays on top of modal */ 
+    <div className="bg-white/70 fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white p-4 rounded shadow-lg relative flex flex-row items-center justify-between gap-5 h-9/10 w-9/10">
+
+        <button
+          onClick={onPrev}
+          className="text-gray-600 hover:text-[#6d8dc2] text-7xl cursor-pointer"
+        >
+          &#8249;
+        </button>
+
+        <div className="flex flex-row items-center gap-5">
+          {/* TODO: make landscape images larger */}
+          <Image
+            src={photo.fullSizeUrl}
+            alt={photo.caption}
+            width={600}
+            height={400}
+            className="rounded object-contain max-h-[80vh] max-w-[80vw]"
+          />
+
+          {/* TODO: gray background for info box (maybe)
+            * TODO: add location and date
+            * TODO: fix font size */}
+          <div className="py-5 self-start">
+            <h2 className="text-[#4a638c] font-semibold text-xl">{photo.caption}</h2>
+            <p className="text-gray-600 text-lg ">{photo.cameraSettings}</p>
+          </div>
+        </div>
+
+        <button
+          onClick={onNext}
+          className="text-gray-600 hover:text-[#6d8dc2] text-7xl cursor-pointer"
+        >
+          &#8250;
+        </button>
+
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-600 hover:text-[#6d8dc2] cursor-pointer font-bold text-2xl"
+        >
+          &#66327;
+        </button>
+
+      </div>
+    </div>
+  );
+}
