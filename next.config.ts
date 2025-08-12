72ec67bbdc16106c9eb22c9ea799fadb090a87b9
Zoe Rizzo <rizzo.zoej@gmail.com>
2025-08-12 19:36:21 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/photography",
+  output: 'export'
 };
 
 export default nextConfig;
