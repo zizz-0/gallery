619865f26dc7048fe12d2ebdf07382005fa593c7
Zoe Rizzo <rizzo.zoej@gmail.com>
2025-08-12 18:12:10 -0400

    home page mostly done on desktop, mobile unfinished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import ScrollHighlights from '@/components/scroll-highlights';
+import Cover from '@/components/cover';
 import photos from '@/data/photos.json';
 
 export default function Home() {
@@ -11,10 +11,15 @@ export default function Home() {
     <main className="bg-white">
       <div className="fixed top-0 right-0 left-0 h-[12vh] bg-[#4f694a] gap-0 p-0 m-0 border-solid border-white border-y-3 flex items-center z-50">
         <h1 className="text-3xl p-5" style={{fontFamily: 'Trebuchet MS, sans-serif'}}>Zoe Rizzo Photography</h1>
-        <a href="https://www.instagram.com/zizz.photography/" target="_blank">
-          <img src="icons/instagram.png" width={40} height={40} className="absolute right-6 top-[3vh]"/>
-        </a>
+        <div className="absolute right-6 top-[3vh] flex items-center">
+          <a href="/" className="mx-4 mr-2 text-2xl underline" style={{fontFamily: 'Trebuchet MS, sans-serif'}}>Home</a>
+          <a href="/gallery" className="mx-4 mr-8 text-2xl underline" style={{fontFamily: 'Trebuchet MS, sans-serif'}}>Gallery</a>
+          <a href="https://www.instagram.com/zizz.photography/" target="_blank">
+            <img src="icons/instagram.png" width={35} height={35}/>
+          </a>
+        </div>
       </div>
+      <Cover/>
       <ScrollHighlights photos={photos}/>
     </main>
   );
--- src/components/scroll-highlights.js
+++ src/components/scroll-highlights.js
@@ -1,20 +1,29 @@
 export default function ScrollHighlights({ photos }) {
-    const highlights = photos.filter(photo => photo.highlight === 'y');
+  const highlights = photos.filter(p => p.highlight === "y");
 
-    return (
-        <div className="w-full h-full bg-white overflow-x-auto mt-[12vh]">
-            <div className="flex flex-row gap-1 p-4">
-                {highlights.map((photo) => (
-                    <div key={photo.id} className="relative h-[300px] min-w-[200px] flex-shrink-0">
-                        <img
-                        src={photo.fullSizeUrl}
-                        alt={photo.caption}
-                        style={{ height: '300px', width: 'auto' }}
-                        className="rounded"
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-[full] min-h-screen mt-[12vh]">
+      <div className="flex flex-wrap gap-0">
+        {highlights.map((photo) => (
+          <div
+            key={photo.id}
+            className="
+              overflow-hidden flex-grow
+              w-screen h-auto max-h-[88vh]
+              sm:min-w-[30vw] sm:h-[88vh] sm:w-auto
+            "
+          >
+            <img
+              src={photo.fullSizeUrl}
+              alt={photo.caption}
+              className="w-full h-full object-cover border-solid border-3"
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center p-5">
+        <h2 className="text-xl text-gray-600" style={{fontFamily: 'Trebuchet MS, sans-serif'}}>View more photos in my <a href="/gallery" className="underline">gallery</a></h2>
+      </div>
+    </div>
+  );
 }
